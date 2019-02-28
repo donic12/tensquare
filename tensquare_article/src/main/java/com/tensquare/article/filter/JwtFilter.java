37b15e775f672a318ccc2adfb82980c2c2b868bb
i9xx <1423202757@qq.com>
2019-02-28 09:25:53 +0800

    .

---
+++ tensquare_article/src/main/java/com/tensquare/article/filter/JwtFilter.java
@@ -0,0 +1,34 @@
+package com.tensquare.article.filter;
+
+import entity.Identity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import util.JwtUtil;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class JwtFilter extends HandlerInterceptorAdapter {
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.err.println("经过了拦截器");
+        final String authorization = request.getHeader("Authorization");
+        if (authorization != null && authorization.startsWith("Bearer ")) {
+            final String token = authorization.substring(7);
+            Identity identity = jwtUtil.parseToken(token);
+            if (identity != null) {
+                if (identity.getRole().equals("USER")) {
+                    request.setAttribute("USER_CLAIMS", identity);
+                } else if (identity.getRole().equals("ADMIN")) {
+                    request.setAttribute("ADMIN_CLAIMS", identity);
+                }
+            }
+        }
+        return true;
+    }
+}
