1336716d715128a799a639287940062b16850051
i9xx <1423202757@qq.com>
2019-03-03 17:44:23 +0800

    .

---
+++ tensquare_manager/src/main/java/com/tensquare/manager/ManagerApplication.java
@@ -0,0 +1,18 @@
+package com.tensquare.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author IceCola
+ */
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+public class ManagerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ManagerApplication.class, args);
+    }
+}
