b6d128ab2f5bb0b93e1dfeed0899dbe96c45e6b8
i9xx <1423202757@qq.com>
2019-03-04 11:42:05 +0800

    .

--- tensquare_base/src/main/java/com/tensquare/base/BaseApplication.java
+++ tensquare_base/src/main/java/com/tensquare/base/BaseApplication.java
@@ -3,6 +3,7 @@ package com.tensquare.base;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +15,6 @@ import util.IdWorker;
  */
 @SpringBootApplication
 @EnableEurekaClient
-@RestController
 public class BaseApplication {
 
     public static void main(String[] args) {
@@ -26,11 +26,4 @@ public class BaseApplication {
         return new IdWorker(1, 1);
     }
 
-    @Value("${test.a}")
-    private String a;
-
-    @GetMapping("/a")
-    public String a() {
-        return a;
-    }
 }
